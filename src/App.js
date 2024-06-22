104e1466508de78c3ac308877a8c03527e8feee2
Alex Mendez <alexmendez@Alexs-MacBook-Pro.local>
2024-06-22 10:41:03 -0700

    add react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DisplayName from './displayname';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayName />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/displayname.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DisplayName() {
+  return <h1>Ricardo Mendez</h1>;
+}
+
+export default DisplayName;
